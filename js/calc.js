90534fe17e3362aae59b950065c74cf1c231195f
jessicashannon <jessdahling@gmail.com>
2015-09-14 13:51:27 -0400

    done

--- js/calc.js
+++ js/calc.js
@@ -6,3 +6,24 @@ $(function(){
 });
 
 // your code here!
+
+function Calculator(){
+  $('#equals').click(validate);
+}
+
+function validate(){
+  var num1 = $('#number1').val();
+  var num2 = $('#number2').val();
+  var operation = $('#operation').val();
+
+  if (isNaN(num1) || isNaN(num2)) {
+    $('#result').text('Sorry, one of those is not a valid number!');
+  }
+  else if(operation != "*" && operation != "+" && operation != "-" && operation != "/"){
+    return $('#result').text('Sorry, not a valid operation!');
+  }
+  else{
+      var total=eval(parseInt(num1)+operation+parseInt(num2));
+      $('#result').text(total);
+      }
+    };
